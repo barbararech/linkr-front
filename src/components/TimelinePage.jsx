dcc5ea34ef49fed76ff0c5d4514e94693ae71d91
barbararech <rech.brb@gmail.com>
2022-08-11 17:28:35 -0300

    fix: fix trending position

--- src/components/TimelinePage.jsx
+++ src/components/TimelinePage.jsx
@@ -1,31 +1,31 @@
-import Header from './Header.jsx';
-import NewPost from './NewPost.jsx';
-import Trending from './Trending.jsx';
-import { Helmet } from 'react-helmet';
-import styled from 'styled-components';
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import { useUserData } from '../contexts/userContext.jsx';
-import { Oval } from 'react-loader-spinner';
+import Header from "./Header.jsx";
+import NewPost from "./NewPost.jsx";
+import Trending from "./Trending.jsx";
+import { Helmet } from "react-helmet";
+import styled from "styled-components";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useUserData } from "../contexts/userContext.jsx";
+import { Oval } from "react-loader-spinner";
 
 export default function TimelinePage() {
   const [posts, setPosts] = useState([]);
   const [refreshAxios, setRefreshAxios] = useState(false);
   const [userData] = useUserData();
-  const [connectError, setConnectError] = useState('');
+  const [connectError, setConnectError] = useState("");
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
   let loadingAnimation = (
     <Oval
       height={80}
       width={80}
-      color='#FFFFFF'
+      color="#FFFFFF"
       wrapperStyle={{}}
-      wrapperClass=''
+      wrapperClass=""
       visible={true}
-      ariaLabel='oval-loading'
-      secondaryColor='#000000'
+      ariaLabel="oval-loading"
+      secondaryColor="#000000"
       strokeWidth={2}
       strokeWidthSecondary={2}
     />
@@ -39,7 +39,7 @@ export default function TimelinePage() {
 
   useEffect(() => {
     const request = axios.get(
-      'https://projeto17-linkr-backend.herokuapp.com/timeline',
+      "https://projeto17-linkr-backend.herokuapp.com/timeline",
       config
     );
     setLoading(true);
@@ -70,27 +70,27 @@ export default function TimelinePage() {
         <div>
           <img src={pictureUrl} />
         </div>
-        <div className='postInfo' id={id}>
+        <div className="postInfo" id={id}>
           <h2>{username}</h2>
           <h3>{article}</h3>
           <div
-            className='urlInfo'
-            style={{ cursor: 'pointer' }}
+            className="urlInfo"
+            style={{ cursor: "pointer" }}
             onClick={() => {
-              window.open(link, '_blank');
+              window.open(link, "_blank");
             }}
           >
             <div>
-              <h3 style={{ fontSize: '16px', marginBottom: '5px' }}>
+              <h3 style={{ fontSize: "16px", marginBottom: "5px" }}>
                 {urlTitle}
               </h3>
-              <h3 style={{ fontSize: '11px', marginBottom: '14px' }}>
+              <h3 style={{ fontSize: "11px", marginBottom: "14px" }}>
                 {urlDescription}
               </h3>
-              <h3 style={{ fontSize: '11px' }}>{link}</h3>
+              <h3 style={{ fontSize: "11px" }}>{link}</h3>
             </div>
             <div>
-              <img src={urlImage} className='urlInfoImg' alt='' />
+              <img src={urlImage} className="urlInfoImg" alt="" />
             </div>
           </div>
         </div>
@@ -98,22 +98,22 @@ export default function TimelinePage() {
     );
   }
 
-  if (connectError !== '') {
+  if (connectError !== "") {
     return (
       <>
         <Helmet>
-          <style>{'body { background-color: #333333; }'}</style>
+          <style>{"body { background-color: #333333; }"}</style>
         </Helmet>
         <Header />
         <Container>
           <NewPost />
           <h1
             style={{
-              color: '#FFFFFF',
-              fontFamily: 'Oswald',
-              marginTop: '100px',
-              textAlign: 'center',
-              fontSize: '40px',
+              color: "#FFFFFF",
+              fontFamily: "Oswald",
+              marginTop: "100px",
+              textAlign: "center",
+              fontSize: "40px",
             }}
           >
             An error occured while trying to fetch the posts, please refresh the
@@ -128,16 +128,16 @@ export default function TimelinePage() {
     return (
       <>
         <Helmet>
-          <style>{'body { background-color: #333333; }'}</style>
+          <style>{"body { background-color: #333333; }"}</style>
         </Helmet>
         <Header />
         <Container>
           <NewPost />
           <h1
             style={{
-              color: '#FFFFFF',
-              fontFamily: 'Oswald',
-              marginTop: '100px',
+              color: "#FFFFFF",
+              fontFamily: "Oswald",
+              marginTop: "100px",
             }}
           >
             There are no posts yet.
@@ -150,57 +150,59 @@ export default function TimelinePage() {
   return (
     <>
       <Helmet>
-        <style>{'body { background-color: #333333; }'}</style>
+        <style>{"body { background-color: #333333; }"}</style>
       </Helmet>
       <Header />
       <Container>
-        <NewPost />
+        <ContainerPosts>
+          <NewPost />
+          {loading ? (
+            <>
+              <h1
+                style={{
+                  color: "#FFFFFF",
+                  fontFamily: "Oswald",
+                  marginTop: "100px",
+                  marginBottom: "10px;",
+                }}
+              >
+                Loading
+              </h1>
+              {loadingAnimation}
+            </>
+          ) : (
+            <>
+              {posts.map(
+                ({
+                  id,
+                  username,
+                  pictureUrl,
+                  link,
+                  article,
+                  urlTitle,
+                  urlDescription,
+                  urlImage,
+                  userId,
+                }) => {
+                  return (
+                    <AllPosts
+                      id={id}
+                      username={username}
+                      pictureUrl={pictureUrl}
+                      link={link}
+                      article={article}
+                      urlTitle={urlTitle}
+                      urlDescription={urlDescription}
+                      urlImage={urlImage}
+                      userId={userId}
+                    />
+                  );
+                }
+              )}
+            </>
+          )}
+        </ContainerPosts>
         <Trending />
-        {loading ? (
-          <>
-            <h1
-              style={{
-                color: '#FFFFFF',
-                fontFamily: 'Oswald',
-                marginTop: '100px',
-                marginBottom: '10px;',
-              }}
-            >
-              Loading
-            </h1>
-            {loadingAnimation}
-          </>
-        ) : (
-          <>
-            {posts.map(
-              ({
-                id,
-                username,
-                pictureUrl,
-                link,
-                article,
-                urlTitle,
-                urlDescription,
-                urlImage,
-                userId,
-              }) => {
-                return (
-                  <AllPosts
-                    id={id}
-                    username={username}
-                    pictureUrl={pictureUrl}
-                    link={link}
-                    article={article}
-                    urlTitle={urlTitle}
-                    urlDescription={urlDescription}
-                    urlImage={urlImage}
-                    userId={userId}
-                  />
-                );
-              }
-            )}
-          </>
-        )}
       </Container>
     </>
   );
@@ -208,12 +210,23 @@ export default function TimelinePage() {
 
 const Container = styled.div`
   margin-top: 185px;
-  width: 100%;
+  /* width: 100%;
+  height: 100vh; */
+  display: flex;
+  /* flex-direction: column; */
+  justify-content: center;
+  box-sizing: border-box;
+`;
+
+const ContainerPosts = styled.div`
+  margin-right: 25px;
+  /* width: 100%; */
   height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   box-sizing: border-box;
+  
 `;
 
 const Posts = styled.div`
@@ -246,13 +259,13 @@ const Posts = styled.div`
     font-size: 20px;
     font-weight: 400;
     color: #ffffff;
-    font-family: 'Lato', sans-serif;
+    font-family: "Lato", sans-serif;
   }
 
   h3 {
     font-size: 18px;
     font-weight: 400;
-    font-family: 'Lato', sans-serif;
+    font-family: "Lato", sans-serif;
     color: #b7b7b7;
   }
 
