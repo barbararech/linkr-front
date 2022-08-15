44faa9cb492ac4c2623cccb33a40335dff173323
ph-ubuntu-pc <phenriquelgsilva@gmail.com>
2022-08-15 12:16:34 -0300

    fix: userPage refresh after redirecting to another userpage

--- src/components/UserPage.jsx
+++ src/components/UserPage.jsx
@@ -1,13 +1,15 @@
 import { useParams } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import HomePage from "../components/shared/HomePage.jsx";
 import axios from "axios";
 import { useUserData } from "../contexts/userContext.jsx";
+import refreshAxiosContext from "../contexts/refreshAxiosContext.jsx";
 
 export default function UserPage() {
   const { id } = useParams();
   const [userData] = useUserData();
   const [username, setUsername] = useState("");
+  const { refreshAxios, setRefreshAxios } = useContext(refreshAxiosContext);
 
   const config = {
     headers: {
@@ -23,11 +25,12 @@ export default function UserPage() {
     request
       .then((response) => {
         setUsername(response.data.username);
+        setRefreshAxios(!refreshAxios)
       })
       .catch((err) => {
         console.error(err);
       });
-  }, []);
+  }, [id]);
 
   const axiosRequest = `user/${id}`;
   const pageName = `${username}'s posts`;
