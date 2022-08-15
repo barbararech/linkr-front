607bd8fdf3860a49f5f03db83f0ae1bf75c97c1a
barbararech <rech.brb@gmail.com>
2022-08-15 15:14:57 -0300

    style: change input position

--- src/components/shared/Input.jsx
+++ src/components/shared/Input.jsx
@@ -1,102 +1,100 @@
 import React, { useContext, useState, useEffect } from "react";
 import styled from "styled-components";
 import axios from "axios";
-import {DebounceInput} from 'react-debounce-input';
+import { DebounceInput } from "react-debounce-input";
 import debounce from "lodash.debounce";
 import { Link } from "react-router-dom";
 
-export default function Input () {
+export default function Input() {
+  const [APIData, setAPIData] = useState([]);
+  const [filteredResults, setFilteredResults] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
 
-    const [APIData, setAPIData] = useState([]);
-    const [filteredResults, setFilteredResults] = useState([]);
-    const [searchInput, setSearchInput] = useState("");
+  useEffect(() => {
+    const promise = axios.get(
+      "https://projeto17-linkr-backend.herokuapp.com/users"
+    );
 
-    useEffect(() => {
-        const promise = axios.get("https://projeto17-linkr-backend.herokuapp.com/users");
-    
-        promise.then((response) => {
-          setAPIData([...response.data]);
-          console.log(APIData)
-        });
+    promise.then((response) => {
+      setAPIData([...response.data]);
+      console.log(APIData);
+    });
+  }, []);
 
-      }, []);
+  const searchItems = (searchValue) => {
+    setSearchInput(searchValue);
+    if (searchInput.length != 0) {
+      const filteredData = APIData.filter((item) => {
+        return Object.values(item.username)
+          .join("")
+          .toLowerCase()
+          .includes(searchInput.toLowerCase());
+      });
+      setFilteredResults(filteredData);
+    } else {
+      //setFilteredResults(APIData);
+    }
+  };
 
-      const searchItems = (searchValue) => {
-        setSearchInput(searchValue);
-        if (searchInput.length != 0) {
-          const filteredData = APIData.filter((item) => {
-            return Object.values(item.username)
-              .join("")
-              .toLowerCase()
-              .includes(searchInput.toLowerCase());
-          });
-          setFilteredResults(filteredData);
-        } else {
-          //setFilteredResults(APIData);
-        }
-      };
+  const updateQuery = (e) => searchItems(e?.target?.value);
+  const debouncedOnChange = debounce(updateQuery, 200);
 
-      const updateQuery = (e) => searchItems(e?.target?.value);
-      const debouncedOnChange = debounce(updateQuery, 200);
+  function Users({ username, pictureUrl }) {
+    return (
+      <UserList>
+        <div className="user">
+          <div>
+            <img src={pictureUrl} alt={"img"} />
+          </div>
 
-function Users({ username, pictureUrl }) {
-        return (
-          <UserList> 
-            <div className="user">
-              <div>
-                <img src={pictureUrl} alt={'img'} />
-              </div>
-      
-              <div className="list">
-                <span className="username">{username}</span>
-              </div>
-            </div>
-          </UserList>
-        );
-      }
+          <div className="list">
+            <span className="username">{username}</span>
+          </div>
+        </div>
+      </UserList>
+    );
+  }
 
-    return(
-        <Container>
-            <div className="split">
-            <input placeholder="Search for people" onChange={debouncedOnChange} />
-            {/* <DebounceInput placeholder="Search for people" debounceTimeout={200} onChange={(e) => searchItems(e.target.value)} /> */}
+  return (
+    <Container>
+      <div className="split">
+        <input placeholder="Search for people" onChange={debouncedOnChange} />
+        {/* <DebounceInput placeholder="Search for people" debounceTimeout={200} onChange={(e) => searchItems(e.target.value)} /> */}
 
-            {searchInput.length > 2
-                ? filteredResults.map((user) => {
-                    return (
-                      <Link to={`/user/${user.id}`}>
-                        <Users
-                        id={user.id}
-                        username={user.username}
-                        pictureUrl={user.pictureUrl}
-                        />
-                      </Link>
-                    );
-                    })
-                : ""}
-
-            </div>
-        </Container>
-    )
+        {searchInput.length > 2
+          ? filteredResults.map((user) => {
+              return (
+                <Link to={`/user/${user.id}`}>
+                  <Users
+                    id={user.id}
+                    username={user.username}
+                    pictureUrl={user.pictureUrl}
+                  />
+                </Link>
+              );
+            })
+          : ""}
+      </div>
+    </Container>
+  );
 }
 
 //Styles
 
-
 const Container = styled.div`
-
   display: flex;
   flex-direction: column;
 
   .split {
-    background-color: #E7E7E7;
+    background-color: #e7e7e7;
     border-radius: 6px;
     display: flex;
     flex-direction: column;
+    z-index: 1;
   }
 
   input {
-    font-family: 'Lato';
+    font-family: "Lato";
     box-sizing: border-box;
     width: 350px;
     min-height: 45px;
@@ -112,7 +110,6 @@ const Container = styled.div`
 `;
 
 const UserList = styled.div`
-
   border-bottom-style: solid;
   border-bottom-color: lightgray;
   padding-bottom: 10px;
@@ -138,14 +135,14 @@ const UserList = styled.div`
   }
 
   .username {
-    font-family: 'Lato';
+    font-family: "Lato";
     font-style: normal;
     font-weight: 400;
     font-size: 19px;
     line-height: 23px;
     color: #515151;
     margin-top: 7px;
-    }
+  }
 
   .list {
     display: flex;
